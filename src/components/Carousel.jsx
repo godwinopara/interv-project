56679a334fc43b59ca1719e7ee20f65c790239d2
Godwin Opara <godwinopara62@yahoo.com>
2023-07-05 19:06:26 +0100

    styled interview project to be responsive on tablet

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -12,7 +12,11 @@ const Carousel = ({ images }) => {
 			slidesPerView={1}
 			pagination={{ clickable: true }}
 			modules={[Pagination]}
-			breakpoints={{ 1024: { slidesPerView: 4, spaceBetween: 20 } }}
+			breakpoints={{
+				768: { slidesPerView: 2, spaceBetween: 20 },
+				1024: { slidesPerView: 3, spaceBetween: 20 },
+				1100: { slidesPerView: 4, spaceBetween: 20 },
+			}}
 			className={styles.image__wrapper}
 		>
 			{images.map((image, id) => {
